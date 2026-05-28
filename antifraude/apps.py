a3c2835dcd989b3a775e76ae702e46901f3c91ca
SeBaS281203 <nsilvajosueseba@uss.edu.pe>
2026-05-28 02:17:54 -0500

    docs(antifraude): agregar docstrings a configuracion de aplicacion

--- antifraude/apps.py
+++ antifraude/apps.py
@@ -1,6 +1,12 @@
+"""
+Configuracion de la aplicacion antifraude.
+"""
 from django.apps import AppConfig
 
 
 class AntifraudeConfig(AppConfig):
+    """
+    Configuracion principal para la app antifraude.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'antifraude'

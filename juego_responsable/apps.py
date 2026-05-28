7d53a710fb480f60440df528fde99888e66b70d5
SeBaS281203 <nsilvajosueseba@uss.edu.pe>
2026-05-28 02:22:24 -0500

    docs(juego_responsable): agregar docstrings a configuracion de aplicacion

--- juego_responsable/apps.py
+++ juego_responsable/apps.py
@@ -1,6 +1,12 @@
+"""
+Configuracion de la aplicacion juego_responsable.
+"""
 from django.apps import AppConfig
 
 
 class JuegoResponsableConfig(AppConfig):
+    """
+    Configuracion principal para la app juego_responsable.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'juego_responsable'
